ed05dbae5fcc9d279b12daabb4b9f965126289fe
rubayet-islam <season.ss2001@gmail.com>
2022-11-25 22:20:17 +0600

    react query used

--- src/components/pages/AllProducts/AllProducts.js
+++ src/components/pages/AllProducts/AllProducts.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
 
 const AllProducts = () => {
-    return (
-        <div>
-            this is all products
-        </div>
-    );
+    const {data: products = []} = useQuery({
+        queryKey: ['products'],
+        queryFn: async () => {
+          const res = fetch("http://localhost:5000/products");
+          const data = await (await res).json();
+          return data;
+        }
+      })
+
+  return <div>there are {products.length} products</div>;
 };
 
 export default AllProducts;
