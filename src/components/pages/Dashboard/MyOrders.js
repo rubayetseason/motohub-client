7d550982ddac0a52cee36c2a62ddaf16e443ebea
rubayet-islam <season.ss2001@gmail.com>
2022-11-26 23:09:45 +0600

    my orders page created

--- src/components/pages/Dashboard/MyOrders.js
+++ src/components/pages/Dashboard/MyOrders.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../context/AuthProvider";
+import Loading from "../../shared/Loading";
+import Order from "./Order";
 
 const MyOrders = () => {
-    return (
-        <div>
-            this is my orders
+  const { user } = useContext(AuthContext);
+  const url = `http://localhost:5000/bookings?email=${user?.email}`;
+  const { data: bookings = [], isLoading } = useQuery({
+    queryKey: ["bookings", user?.email],
+    queryFn: async () => {
+      const res = await fetch(url);
+      const data = await res.json();
+      return data;
+    },
+  });
+
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
+
+  return (
+    <div>
+      <div>
+        <h1 className="text-4xl font-bold mt-10 mb-5">
+          You have {bookings.length} orders
+        </h1>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 px-5 my-5">
+          {bookings.map((booking) => (
+            <Order key={booking._id} booking={booking}></Order>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default MyOrders;
---
+++ src/components/pages/Dashboard/Order.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { GoVerified } from "react-icons/go";
+
+const Order = ({ booking }) => {
+  const { product, image, resale_price, seller, verified, customer, catagory} = booking;
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl image-full">
+      <figure>
+        <img src={image} className="w-full object-cover" alt="product" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{product}</h2>
+        <p className="text-start font-semibold">Category: {catagory}</p>
+        <p className="text-start font-semibold">Price: {resale_price}</p>
+        <p className="text-start font-semibold">Customer: {customer}</p>
+        <p className="text-start font-semibold">Seller: {seller}{verified ? (
+            <span className="badge badge-lg">
+              <GoVerified />
+            </span>
+          ) : (
+            ""
+          )}</p>
+        <div className="card-actions justify-start">
+        <button className="btn glass btn btn-sm">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Order;
