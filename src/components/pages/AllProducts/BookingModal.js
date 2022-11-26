0f2c3d905c971ceed9938cb53229835055aa57c8
rubayet-islam <season.ss2001@gmail.com>
2022-11-26 13:51:59 +0600

    modal data gotten from customer

--- src/components/pages/AllProducts/BookingModal.js
+++ src/components/pages/AllProducts/BookingModal.js
@@ -2,8 +2,28 @@ import React, { useContext } from "react";
 import { AuthContext } from "../../../context/AuthProvider";
 
 const BookingModal = ({ item, setItem }) => {
-  const { name, catagory, resale_price, seller, location } = item;
+  const { name, catagory, resale_price, seller, _id } = item;
   const { user } = useContext(AuthContext);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const form = event.target;
+    const customer = form.customer.value;
+    const email = form.email.value;
+    const phone = form.phone.value;
+    const obj = {
+      customer,
+      email,
+      phone,
+      product: name,
+      product_id: _id,
+      catagory,
+      resale_price,
+      seller,
+    };
+    console.log(obj);
+  };
+
   return (
     <div>
       <input type="checkbox" id="bookingModal" className="modal-toggle" />
@@ -15,19 +35,9 @@ const BookingModal = ({ item, setItem }) => {
           >
             ✕
           </label>
-          <h3 className="text-lg font-bold">{name}</h3>
-          <form>
-            <div className="form-control w-full">
-              <label className="label">
-                <span className="label-text">Resale Price</span>
-              </label>
-              <input
-                type="text"
-                disabled
-                value={resale_price}
-                className="input input-bordered input-secondary w-full"
-              />
-            </div>
+          <form onSubmit={handleSubmit}>
+            <h3 className="text-lg font-bold">{name}</h3>
+            <p>Resale Price: {resale_price}</p>
             <div className="form-control w-full">
               <label className="label">
                 <span className="label-text">Your Name</span>
@@ -35,6 +45,7 @@ const BookingModal = ({ item, setItem }) => {
               <input
                 type="text"
                 disabled
+                name="customer"
                 value={user?.displayName}
                 className="input input-bordered input-secondary w-full"
               />
@@ -45,6 +56,7 @@ const BookingModal = ({ item, setItem }) => {
               </label>
               <input
                 type="text"
+                name="email"
                 value={user?.email}
                 disabled
                 className="input input-bordered input-secondary w-full"
@@ -56,6 +68,7 @@ const BookingModal = ({ item, setItem }) => {
               </label>
               <input
                 type="number"
+                name="phone"
                 placeholder="+880**********"
                 className="input input-bordered input-secondary w-full"
               />
@@ -66,6 +79,7 @@ const BookingModal = ({ item, setItem }) => {
               </label>
               <input
                 type="text"
+                name="meeting"
                 placeholder="Enter meeting location"
                 className="input input-bordered input-secondary w-full"
               />
