72175ffa56ab5e41d3667c23fcb039975c551c71
rubayet-islam <season.ss2001@gmail.com>
2022-11-26 13:56:12 +0600

    modal added to catagory route as well

--- src/components/pages/AllProducts/Catagory.js
+++ src/components/pages/AllProducts/Catagory.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
+import BookingModal from "./BookingModal";
 import Product from "./Product";
 
 const Catagory = () => {
+  const [item, setItem] = useState(null);
   const products = useLoaderData();
 const prod = products[0].catagory;
   return (
@@ -10,8 +12,13 @@ const prod = products[0].catagory;
       <h1 className="text-4xl md:text-5xl font-bold my-5">Category: {prod}</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 my-10 px-5">
         {products.map((product) => (
-          <Product key={product._id} product={product}></Product>
+          <Product key={product._id} product={product}  setItem={setItem}></Product>
         ))}
+      </div>
+      <div>
+{
+  item && <BookingModal item={item} setItem={setItem}></BookingModal>
+}
       </div>
     </div>
   );
