cd0e316d703decff2e97123519487f731c62ce14
rubayet-islam <season.ss2001@gmail.com>
2022-11-28 10:48:00 +0600

    wishlist part done

--- src/components/pages/Dashboard/MyWishlist.js
+++ src/components/pages/Dashboard/MyWishlist.js
@@ -1,9 +1,13 @@
 import { useQuery } from "@tanstack/react-query";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../../context/AuthProvider";
+import Loading from "../../shared/Loading";
+import BookingModal from "../AllProducts/BookingModal";
+import Product from "../AllProducts/Product";
 
 const MyWishlist = () => {
   const { user } = useContext(AuthContext);
+  const [item, setItem] = useState(null);
 
   const { data: wishlists = [], isLoading } = useQuery({
     queryKey: ["wishlist"],
@@ -13,11 +17,27 @@ const MyWishlist = () => {
       return data;
     },
   });
-  console.log(wishlists);
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
 
   return (
     <div>
-      <h1 className="text-4xl font-bold">Welcome to your wishlist</h1>
+      <h1 className="text-4xl mt-16 mb-10 font-bold">
+        Welcome to your wishlist
+      </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {wishlists.map((wishlist) => (
+          <Product
+            key={wishlist._id}
+            setItem={setItem}
+            product={wishlist}
+          ></Product>
+        ))}
+      </div>
+      <div>
+        {item && <BookingModal item={item} setItem={setItem}></BookingModal>}
+      </div>
     </div>
   );
 };
